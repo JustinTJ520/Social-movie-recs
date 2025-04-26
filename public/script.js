f81467d24a197f08ac93fd0f96112a19d6610ecc
Justin <jjor398@wgu.edu>
2025-04-26 16:47:11 -0400

    Added .gitignore for sensitive data

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 const apiBaseUrl = "https://api.themoviedb.org/3";
-const apiKey = "cb8a14125765a6424087772a479efd86";
 const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const moviesGrid = document.getElementById("movies-grid");
